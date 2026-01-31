97a9d2459045bbd49933a916858a28e2790e1e00
Sahej <sahejdhingra6@gmail.com>
2026-01-31 15:29:11 +0530

    fix: wrap GlobalStyles component with FullScreenGate for proper rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,7 +7,9 @@ import { GlobalStyles } from './components/GlobalStyles.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
+    <FullScreenGate>
+      <GlobalStyles />
       <App />
+    </FullScreenGate>
   </React.StrictMode>,
 );
